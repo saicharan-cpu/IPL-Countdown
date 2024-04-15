c42b35c1c1043123c96fa768e8483d62bcbeddb4
Nishitha <madhu.ni@northeastern.edu>
2024-04-15 02:06:40 -0400

    fixes

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,9 @@
-import mysql.connector
 from flask import Flask
 from app import config  
 
 
+db_connection = None
+db_cursor = None
 app = Flask(__name__)
 app.config.from_object(config)
-
-
-db_connection = mysql.connector.connect(**app.config['DB_CONFIG'])
-
-db_cursor = db_connection.cursor()
-
-
-@app.teardown_appcontext
-def close_db_connection(exception=None):
-    db_connection.close()
 
